3793a9a7fd042c7b8255607758f439c444063686
Ioann <ioan@mail.cru>
2021-08-28 12:04:59 +0500

    Rating

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Movie, Category, Review
+from .models import Movie, Category, Review, Rating
 
 
 class CategorySerializers(serializers.ModelSerializer):
@@ -73,4 +73,21 @@ class MovieDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         exclude = ("draft", )
+
+
+class CreateRatingSerializer(serializers.ModelSerializer):
+    """Добавление рейтинга пользователем"""
+
+    class Meta:
+        model = Rating
+        fields = ('star', "movie")
+
+    def create(self, validated_data):
+        rating = Rating.objects.update_or_create(
+            ip=validated_data.get('ip', None),
+            movie=validated_data.get('movie', None),
+            defaults={'star': validated_data.get("star")}
+
+        )
+        return rating
 
